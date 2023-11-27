012c53e9f04b1f78fd991ae53a24c96b64775124
Rong <93319350+michellerong@users.noreply.github.com>
2023-11-27 10:12:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,18 @@ app = FastAPI()
 def root():
     return homework
 
+@app.get('/school')
+def get_homeworks_by_school(school: str = "") -> dict:
+    return homework[school]
+    
+
+
+@app.get('/semester')
+def get_homeworks_by_semester(semester: str = "") -> list:
+    ntue = homework["ntue"][semester]
+    ntut = homework["ntut"][semester]
+
+    return [*ntue, *ntut]
+
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5000, reload=True)
